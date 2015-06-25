3cb1d9e9ad1d85d59742cb4b196095ac8cd854f0
unknown <ksjdragon@gmail.com>
2015-06-25 03:34:14 -0400

    added teams and death catching, but  doesn't disable game yet

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 var coordinate;
 var username;
+var team;
 var timer;
 document.getElementsByClassName('play')[0].onclick = function startGame() {
 	username = document.createTextNode(document.getElementsByClassName('username')[0].value)
+    /* get this from server later */
 	coordinate = [0,0]
+    team = "red"
+    /* end */
+
 	// Server Stuff, not necessary now.
 	// var ip = document.getElementsByClassName('ip')[0].value;
 	// console.log(ip);
 	// console.log(username);
 	// if (ip.match(/[a-z]/i) /* && list of ips */ || ip === "") {
 	// 	alert("That wasn't a valid ip, so we picked a random one for you!");
-	// 	/*
+	// 	/* 
 	// 	ip = retrieveServerIPs('array')[Math.floor((Math.random() * retrieveServerIPs('amount')) + 1)];
 	// 	*/
 	// }
@@ -44,21 +49,27 @@ function tableCreate() {
 }
 //Creation of Player
 function createPlayer() {
-	table.rows[coordinate[0]].cells[coordinate[1]].style.backgroundColor = "red"
-	table.rows[coordinate[0]].cells[coordinate[1]].className = "player"
-	table.rows[coordinate[0]].cells[coordinate[1]].appendChild(username)
+	table.rows[coordinate[0]].cells[coordinate[1]].style.backgroundColor = team;
+	table.rows[coordinate[0]].cells[coordinate[1]].className = "player";
+	table.rows[coordinate[0]].cells[coordinate[1]].appendChild(username);
 }
-
-//add argument "team" to differentiate what color to set
 function movement(x,y) {
     timer = 
         setTimeout(function() {
             try {
+                if(table.rows[coordinate[0] + y].cells[coordinate[1] + x].id === team) {
+                    console.log('you died');
+                }
             	table.rows[coordinate[0]].cells[coordinate[1]].className = "";
             	table.rows[coordinate[0] + y].cells[coordinate[1] + x].className = "player";
-            	document.getElementsByClassName('player')[0].style.backgroundColor = "red";
+                table.rows[coordinate[0] + y].cells[coordinate[1] + x].id = team; 
+            	document.getElementsByClassName('player')[0].style.backgroundColor = team;
                 document.getElementsByClassName('player')[0].appendChild(username);
-            	table.rows[coordinate[0]].cells[coordinate[1]].style.backgroundColor = "#FC9D9D";
+                if(team === "red") {
+            	   table.rows[coordinate[0]].cells[coordinate[1]].style.backgroundColor = "#FC9D9D";
+                } else if(team === "blue") {
+                    table.rows[coordinate[0]].cells[coordinate[1]].style.backgroundColor = "some other color that is a blue";
+                }
             	coordinate = [coordinate[0] + y, coordinate[1] + x];
                 movement(x,y);
             }
@@ -74,7 +85,7 @@ function movePlayer(e) {
     e = e || window.event;
 
     switch(e.keyCode) {
-    	//up
+    	//up   
     	case 38:
             clearTimeout(timer);
     		movement(0,-1);
