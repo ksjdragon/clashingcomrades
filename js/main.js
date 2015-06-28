b00ee47d731200eb42d6cb39dc4af1937bb6d741
Kenneth Jao <ksjdragon@gmail.com>
2015-06-28 04:54:35 -0400

    fixed freezing on keydown, different background color

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
+/*Global Variables */
 var coordinate;
 var username;
 var team;
 var timer;
+var type;
+/* End */
+
 document.getElementsByClassName('play')[0].onclick = function startGame() {
     username = document.createTextNode(document.getElementsByClassName('username')[0].value)
     /* get this from server later */
@@ -15,7 +19,7 @@ document.getElementsByClassName('play')[0].onclick = function startGame() {
     // console.log(username);
     // if (ip.match(/[a-z]/i) /* && list of ips */ || ip === "") {
     //  alert("That wasn't a valid ip, so we picked a random one for you!");
-    //  /* 
+    //  /*
     //  ip = retrieveServerIPs('array')[Math.floor((Math.random() * retrieveServerIPs('amount')) + 1)];
     //  */
     // }
@@ -30,9 +34,12 @@ document.getElementsByClassName('play')[0].onclick = function startGame() {
     /*
     connectServer(ip);
     */
-} 
+}
 
-//Creation of Table
+/*********************/
+/* Creation of Table */
+/*********************/
+
 function tableCreate() {
     var body = document.body
     var tbl  = document.createElement('table');
@@ -47,14 +54,19 @@ function tableCreate() {
     body.appendChild(tbl);
     table = document.getElementsByTagName('table')[0];
 }
-//Creation of Player
+
+/**********************/
+/* Creation of Player */
+/**********************/
+
 function createPlayer() {
     table.rows[coordinate[0]].cells[coordinate[1]].style.backgroundColor = team;
     table.rows[coordinate[0]].cells[coordinate[1]].className = "player";
     table.rows[coordinate[0]].cells[coordinate[1]].appendChild(username);
 }
+/* Put this stuff server side to prevent H4X (Arav) later */
 function movement(x,y) {
-    timer = 
+    timer =
         setTimeout(function() {
             try {
                 if (table.rows[coordinate[0] + y].cells[coordinate[1] + x].id === team) {
@@ -66,7 +78,7 @@ function movement(x,y) {
                 else {
                     table.rows[coordinate[0]].cells[coordinate[1]].className = "";
                     table.rows[coordinate[0] + y].cells[coordinate[1] + x].className = "player";
-                    table.rows[coordinate[0] + y].cells[coordinate[1] + x].id = team; 
+                    table.rows[coordinate[0] + y].cells[coordinate[1] + x].id = team;
                     document.getElementsByClassName('player')[0].style.backgroundColor = team;
                     document.getElementsByClassName('player')[0].appendChild(username);
                     if(team === "red") {
@@ -82,37 +94,30 @@ function movement(x,y) {
             catch(err) {
             }
         }, 150);
-
 }
 document.onkeydown = movePlayer;
-var lose = 0;
-/* Put this stuff server side to prevent H4X (Arav) later */
-function movePlayer(e) { 
+
+function movePlayer(e) {
 
     e = e || window.event;
 
-    switch(e.keyCode) {
-        //up   
-        case 38:
-            clearTimeout(timer);
-            movement(0,-1);
-            break;
-        //down
-        case 40:
-            clearTimeout(timer);
-            movement(0,1);
-            break;
-        //left
-        case 37:
-            clearTimeout(timer);
-            movement(-1,0);
-            break;
-        //right
-        case 39:
-            clearTimeout(timer);
-            movement(1,0);
-            break;
-        default:
-            break;
+    if(e.keyCode === 38 && type != "up") {
+        type = "up";
+        clearTimeout(timer);
+        movement(0,-1);
+    } else if(e.keyCode === 40 && type != "down") {
+        type = "down";
+        clearTimeout(timer);
+        movement(0,1);
+    }
+    else if(e.keyCode === 37 && type != "left") {
+        type = "left"
+        clearTimeout(timer);
+        movement(-1,0);
+    }
+    else if(e.keyCode === 39 && type != "right") {
+        type = "right"
+        clearTimeout(timer);
+        movement(1,0);
     }
 }
